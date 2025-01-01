5ef28256f1c89a7125c042255829122a51bac4e6
Geoff Seemueller <gseemueller@gmail.com>
2025-01-01 06:18:11 -0500

    cleanup comments index.ts

--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,12 @@ import {HyphalObject} from "./hyphal_object";
 export {HyphalObject};
 
 export default {
-	/**
-	 * This is the standard fetch handler for a Cloudflare Worker
-	 *
-	 * @param request - The request submitted to the Worker from the client
-	 * @param env - The interface to reference bindings declared in wrangler.toml
-	 * @param ctx - The execution context of the Worker
-	 * @returns The response to be sent back to the client
-	 */
 	async fetch(request, env, ctx): Promise<Response> {
 
 		let id: DurableObjectId = env.HYPHAL_OBJECT.idFromName(new URL(request.url).pathname);
 
-		// This stub creates a communication channel with the Durable Object instance
-		// The Durable Object constructor will be invoked upon the first call for a given id
 		let stub = env.HYPHAL_OBJECT.get(id);
 
-		// We call the `sayHello()` RPC method on the stub to invoke the method on the remote
-		// Durable Object instance
 		return stub.fetch(request);
 	},
 } satisfies ExportedHandler<Env>;
